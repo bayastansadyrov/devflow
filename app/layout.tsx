564e477b49f519d0380f15a97a2434575b8d9dc9
Bayastan <bayas.sadyrov.dev@gmail.com>
2025-10-24 10:47:47 +0200

    Light & Dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/context/Theme";
+import Navbar from "@/components/navigation/navbar";
 
 const inter = Inter({
    variable: "--font-inter",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <html lang="en" suppressHydrationWarning>
          <body className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}>
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+               <Navbar />
                {children}
             </ThemeProvider>
          </body>
