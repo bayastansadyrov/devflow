bae70ea01a22f024c1be1bc8f43d45887c015711
Bayastan <bayas.sadyrov.dev@gmail.com>
2025-10-25 15:49:12 +0200

    Google | github auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/context/Theme";
-import Navbar from "@/components/navigation/navbar";
+import { Toaster } from "sonner";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
 
 const inter = Inter({
    variable: "--font-inter",
@@ -21,19 +23,29 @@ export const metadata: Metadata = {
       icon: "/images/site-logo.svg",
    },
 };
-export default function RootLayout({
+export default async function RootLayout({
    children,
 }: Readonly<{
    children: React.ReactNode;
 }>) {
+   const session = await auth();
+
    return (
       <html lang="en" suppressHydrationWarning>
-         <body className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}>
-            <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-               <Navbar />
-               {children}
-            </ThemeProvider>
-         </body>
+         <SessionProvider session={session}>
+            <body className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}>
+               <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+                  {children}
+               </ThemeProvider>
+               <Toaster />
+            </body>
+         </SessionProvider>
       </html>
    );
 }
+
+// "packageManager": "npm@10.7.0",
+// "overrides": {
+//    "react": "$react",
+//    "react-dom": "$react-dom"
+// }
