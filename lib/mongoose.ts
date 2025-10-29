24679833365951367935443f70e3d6f28b1ffead
Bayastan <bayas.sadyrov.dev@gmail.com>
2025-10-29 15:30:17 +0100

    Implemented Logging

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,9 +1,12 @@
-import { error } from "console";
 import mongoose, { Mongoose } from "mongoose";
+
+import logger from "./logger";
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
-if (!MONGODB_URI) throw new Error("MONGODB_URI is not defined");
+if (!MONGODB_URI) {
+   throw new Error("MONGODB_URI is not defined");
+}
 
 interface MongooseCache {
    conn: Mongoose | null;
@@ -11,6 +14,7 @@ interface MongooseCache {
 }
 
 declare global {
+   // eslint-disable-next-line no-var
    var mongoose: MongooseCache;
 }
 
@@ -21,7 +25,10 @@ if (!cached) {
 }
 
 const dbConnect = async (): Promise<Mongoose> => {
-   if (cached.conn) return cached.conn;
+   if (cached.conn) {
+      logger.info("Using existing mongoose connection");
+      return cached.conn;
+   }
 
    if (!cached.promise) {
       cached.promise = mongoose
@@ -29,11 +36,11 @@ const dbConnect = async (): Promise<Mongoose> => {
             dbName: "devflow",
          })
          .then((result) => {
-            console.log("Connected to MongoDB");
+            logger.info("Connected to MongoDB");
             return result;
          })
          .catch((error) => {
-            console.error("Error connecting to Mongo DB", error);
+            logger.error("Error connecting to MongoDB", error);
             throw error;
          });
    }
