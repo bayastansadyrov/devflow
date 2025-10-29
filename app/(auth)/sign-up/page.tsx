29bffe30101cd9136eb7831db08e24c83bca300a
Bayastan <bayas.sadyrov.dev@gmail.com>
2025-10-30 00:04:52 +0100

    Implement Create & Read user routes

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import AuthForm from "@/components/forms/AuthForm";
-import { SignUpSchema } from "@/lib/validation";
+import { SignUpSchema } from "@/lib/validations";
 
 export default function SignUp() {
    return (
--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
-import { AskQuestionSchema } from "@/lib/validation";
+import { AskQuestionSchema } from "@/lib/validations";
 
 import { Button } from "../ui/button";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
