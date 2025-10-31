5fb3d55c24986c1cc63989d4904edc4ba89e00ca
Bayastan <bayas.sadyrov.dev@gmail.com>
2025-10-31 14:48:35 +0100

    Implement action handler

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -5,7 +6,6 @@ import ROUTES from "@/constants/routes";
 import { getDeviconClassName } from "@/lib/utils";
 
 import { Badge } from "../ui/badge";
-import Image from "next/image";
 
 interface Props {
    _id: string;
@@ -18,16 +18,16 @@ interface Props {
    handleRemove?: () => void;
 }
 
-const handleClick = (e: React.MouseEvent) => {
-   e.preventDefault();
-};
-
 const TagCard = ({ _id, name, questions, showCount, compact, remove, isButton, handleRemove }: Props) => {
    const iconClass = getDeviconClassName(name);
 
+   const handleClick = (e: React.MouseEvent) => {
+      e.preventDefault();
+   };
+
    const Content = (
       <>
-         <Badge className="subtle-medium background-light800_dark300 text-light400_light500 uppercasef flex flex-row gap-2 rounded-md border-none px-4 py-2">
+         <Badge className="subtle-medium background-light800_dark300 text-light400_light500 flex flex-row gap-2 rounded-md border-none px-4 py-2 uppercase">
             <div className="flex-center space-x-2">
                <i className={`${iconClass} text-sm`}></i>
                <span>{name}</span>
@@ -36,11 +36,11 @@ const TagCard = ({ _id, name, questions, showCount, compact, remove, isButton, h
             {remove && (
                <Image
                   src="/icons/close.svg"
-                  alt="close icon"
                   width={12}
                   height={12}
-                  onClick={handleRemove}
+                  alt="close icon"
                   className="cursor-pointer object-contain invert-0 dark:invert"
+                  onClick={handleRemove}
                />
             )}
          </Badge>
@@ -55,7 +55,9 @@ const TagCard = ({ _id, name, questions, showCount, compact, remove, isButton, h
             {Content}
          </button>
       ) : (
-         <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2"></Link>
+         <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
+            {Content}
+         </Link>
       );
    }
 };
