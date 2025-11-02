cb3aabdaba87bdf083d12180612ee06df8270eb8
Bayastan <bayas.sadyrov.dev@gmail.com>
2025-11-02 12:26:42 +0100

    Implement Fetch and Display Questions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
-import { techMap } from "@/constants/techMap";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+
+import { techMap } from "@/constants/techMap";
 
 export function cn(...inputs: ClassValue[]) {
    return twMerge(clsx(inputs));
@@ -12,8 +13,10 @@ export const getDeviconClassName = (techName: string) => {
    return techMap[normalizedTechName] ? `${techMap[normalizedTechName]} colored` : "devicon-devicon-plain";
 };
 
-export const getTimeStamp = (date: Date) => {
+export const getTimeStamp = (createdAt: Date) => {
+   const date = new Date(createdAt);
    const now = new Date();
+
    const secondsAgo = Math.floor((now.getTime() - date.getTime()) / 1000);
 
    const units = [
