6a427a05b6cc4778059218c648a1c9dd6aca9f72
Bayastan <bayas.sadyrov.dev@gmail.com>
2025-11-02 20:23:36 +0100

    Implement getTags server action

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,8 @@ import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 import { getQuestions } from "@/lib/actions/question.action";
+import DataRenderer from "@/components/DataRenderer";
+import { EMPTY_QUESTION } from "@/constants/states";
 
 interface SearchParams {
    searchParams: Promise<{ [key: string]: string }>;
@@ -35,21 +37,20 @@ const Home = async ({ searchParams }: SearchParams) => {
             <LocalSearch route="/" imgSrc="/icons/search.svg" placeholder="Search questions..." otherClasses="flex-1" />
          </section>
          <HomeFilter />
-         {success ? (
-            <div className="mt-10 flex w-full flex-col gap-6">
-               {questions && questions.length > 0 ? (
-                  questions.map((question) => <QuestionCard key={question._id} question={question} />)
-               ) : (
-                  <div className="mt-10 flex w-full items-center justify-center">
-                     <p className="text-dark400_light700">No questions found</p>
-                  </div>
-               )}
-            </div>
-         ) : (
-            <div className="mt-10 flex w-full items-center justify-center">
-               <p className="text-dark400_light700">{error?.message || "Failed to fetch questions"}</p>
-            </div>
-         )}
+
+         <DataRenderer
+            success={success}
+            error={error}
+            data={questions}
+            empty={EMPTY_QUESTION}
+            render={(questions) => (
+               <div className="mt-10 flex w-full flex-col gap-6">
+                  {questions.map((question) => (
+                     <QuestionCard key={question._id} question={question} />
+                  ))}
+               </div>
+            )}
+         />
       </>
    );
 };
