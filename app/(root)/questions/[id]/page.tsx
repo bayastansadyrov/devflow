cac77d201c088d6d372a89c2e72575004d864604
Bayastan <bayas.sadyrov.dev@gmail.com>
2025-11-05 15:34:32 +0100

    Show User Has Saved Question

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -15,6 +15,7 @@ import { getQuestion, incrementViews } from "@/lib/actions/question.action";
 import { hasVoted } from "@/lib/actions/vote.action";
 import { formatNumber, getTimeStamp } from "@/lib/utils";
 import SaveQuestion from "@/components/questions/SaveQuestion";
+import { hasSavedQuestion } from "@/lib/actions/collection.action";
 
 const QuestionDetails = async ({ params }: RouteParams) => {
    const { id } = await params;
@@ -42,6 +43,10 @@ const QuestionDetails = async ({ params }: RouteParams) => {
       targetType: "question",
    });
 
+   const hasSavedQuestionPromise = hasSavedQuestion({
+      questionId: question._id,
+   });
+
    const { author, createdAt, answers, views, tags, content, title } = question;
 
    return (
@@ -60,7 +65,7 @@ const QuestionDetails = async ({ params }: RouteParams) => {
                   </Link>
                </div>
 
-               <div className="flex justify-end">
+               <div className="flex items-center justify-end gap-4">
                   <Suspense fallback={<div>Loading...</div>}>
                      <Votes
                         targetType="question"
@@ -72,7 +77,7 @@ const QuestionDetails = async ({ params }: RouteParams) => {
                   </Suspense>
 
                   <Suspense fallback={<div>Loading...</div>}>
-                     <SaveQuestion questionId={question._id} />
+                     <SaveQuestion questionId={question._id} hasSavedQuestionPromise={hasSavedQuestionPromise} />
                   </Suspense>
                </div>
             </div>
