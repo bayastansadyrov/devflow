6001f29cae7fb0b826ae9327525be07957256287
Bayastan <bayas.sadyrov.dev@gmail.com>
2025-11-05 01:10:45 +0100

    Add votes func

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { after } from "next/server";
-import React from "react";
-
+import { Suspense } from "react";
+import AllAnswers from "@/components/answers/AllAnswers";
 import TagCard from "@/components/cards/TagCard";
 import { Preview } from "@/components/editor/Preview";
 import AnswerForm from "@/components/forms/AnswerForm";
 import Metric from "@/components/Metric";
 import UserAvatar from "@/components/UserAvatar";
+import Votes from "@/components/votes/Votes";
 import ROUTES from "@/constants/routes";
 import { getAnswers } from "@/lib/actions/answer.action";
 import { getQuestion, incrementViews } from "@/lib/actions/question.action";
+import { hasVoted } from "@/lib/actions/vote.action";
 import { formatNumber, getTimeStamp } from "@/lib/utils";
-import AllAnswers from "@/components/answers/AllAnswers";
-import Votes from "@/components/votes/Votes";
 
 const QuestionDetails = async ({ params }: RouteParams) => {
    const { id } = await params;
@@ -36,7 +36,10 @@ const QuestionDetails = async ({ params }: RouteParams) => {
       filter: "latest",
    });
 
-   console.log("ANSWERS", answersResult);
+   const hasVotedPromise = hasVoted({
+      targetId: question._id,
+      targetType: "question",
+   });
 
    const { author, createdAt, answers, views, tags, content, title } = question;
 
@@ -57,12 +60,15 @@ const QuestionDetails = async ({ params }: RouteParams) => {
                </div>
 
                <div className="flex justify-end">
-                  <Votes
-                     upvotes={question.upvotes}
-                     hasupVoted={true}
-                     downvotes={question.downvotes}
-                     hasdownVoted={false}
-                  />
+                  <Suspense fallback={<div>Loading...</div>}>
+                     <Votes
+                        targetType="question"
+                        upvotes={question.upvotes}
+                        downvotes={question.downvotes}
+                        targetId={question._id}
+                        hasVotedPromise={hasVotedPromise}
+                     />
+                  </Suspense>
                </div>
             </div>
 
