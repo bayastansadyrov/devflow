1b2d33edeb6ea19bea3e5a9019a443ca1abc96dd
Bayastan <bayas.sadyrov.dev@gmail.com>
2025-11-08 18:22:14 +0100

    react cache

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -3,7 +3,6 @@
 import mongoose, { FilterQuery, Types } from "mongoose";
 import { revalidatePath } from "next/cache";
 import { after } from "next/server";
-
 import { auth } from "@/auth";
 import { Answer, Collection, Interaction, Vote } from "@/database";
 import Question, { IQuestionDoc } from "@/database/question.model";
@@ -19,9 +18,9 @@ import {
    IncrementViewsSchema,
    PaginatedSearchParamsSchema,
 } from "@/lib/validations";
-
 import dbConnect from "../mongoose";
 import { createInteraction } from "./interaction.action";
+import { cache } from "react";
 
 export async function createQuestion(params: CreateQuestionParams): Promise<ActionResponse<Question>> {
    const validationResult = await action({
@@ -175,7 +174,9 @@ export async function editQuestion(params: EditQuestionParams): Promise<ActionRe
    }
 }
 
-export async function getQuestion(params: GetQuestionParams): Promise<ActionResponse<Question>> {
+export const getQuestion = cache(async function getQuestion(
+   params: GetQuestionParams
+): Promise<ActionResponse<Question>> {
    const validationResult = await action({
       params,
       schema: GetQuestionSchema,
@@ -198,7 +199,7 @@ export async function getQuestion(params: GetQuestionParams): Promise<ActionResp
    } catch (error) {
       return handleError(error) as ErrorResponse;
    }
-}
+});
 
 export async function getRecommendedQuestions({ userId, query, skip, limit }: RecommendationParams) {
    const interactions = await Interaction.find({
