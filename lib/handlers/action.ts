fe68cc5adcf3be1fb695a9ebd022fff74b71a447
Bayastan <bayas.sadyrov.dev@gmail.com>
2025-11-08 13:35:35 +0100

    Implement User Answers Tab

--- lib/handlers/action.ts
+++ lib/handlers/action.ts
@@ -14,6 +14,11 @@ type ActionOptions<T> = {
    authorize?: boolean;
 };
 
+// 1. Checking whether the schema and params are provided and validated.
+// 2. Checking whether the user is authorized.
+// 3. Connecting to the database.
+// 4. Returning the params and session.
+
 async function action<T>({ params, schema, authorize = false }: ActionOptions<T>) {
    if (schema && params) {
       try {
