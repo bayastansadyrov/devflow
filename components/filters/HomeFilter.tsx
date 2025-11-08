19da1026802f97db6349601e6c5eec40a370a261
Bayastan <bayas.sadyrov.dev@gmail.com>
2025-11-08 18:00:41 +0100

    Build Recommendation System

--- components/filters/HomeFilter.tsx
+++ components/filters/HomeFilter.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { useSearchParams, useRouter } from "next/navigation";
-import { useState } from "react";
+import React, { useState } from "react";
+
 import { formUrlQuery, removeKeysFromUrlQuery } from "@/lib/url";
 import { cn } from "@/lib/utils";
+
 import { Button } from "../ui/button";
 
 const filters = [
-   { name: "React", value: "react" },
-   { name: "JavaScript", value: "javascript" },
+   { name: "Newest", value: "newest" },
+   { name: "Popular", value: "popular" },
+   { name: "Unanswered", value: "unanswered" },
+   { name: "Recommeded", value: "recommended" },
 ];
 
 const HomeFilter = () => {
