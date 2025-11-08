14204444bda92a7c7a7a582d65469f7ca98e4b16
Bayastan <bayas.sadyrov.dev@gmail.com>
2025-11-08 18:07:37 +0100

    Implement Loading to Community page

--- components/filters/HomeFilter.tsx
+++ components/filters/HomeFilter.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSearchParams, useRouter } from "next/navigation";
-import React, { useState } from "react";
+import { useState } from "react";
 
 import { formUrlQuery, removeKeysFromUrlQuery } from "@/lib/url";
 import { cn } from "@/lib/utils";
