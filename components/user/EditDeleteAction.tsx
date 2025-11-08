bbce9aef6b37687f8e4bd5609b7ff2281641f86c
Bayastan <bayas.sadyrov.dev@gmail.com>
2025-11-08 17:12:17 +0100

    Answer Delete Func

--- components/user/EditDeleteAction.tsx
+++ components/user/EditDeleteAction.tsx
@@ -14,6 +14,7 @@ import {
 import { toast } from "sonner";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { deleteAnswer, deleteQuestion } from "@/lib/actions/question.action";
 
 interface Props {
    type: string;
@@ -30,10 +31,12 @@ const EditDeleteAction = ({ type, itemId }: Props) => {
    const handleDelete = async () => {
       if (type === "Question") {
          // Call API to delete question
+         await deleteQuestion({ questionId: itemId });
 
          toast.success("Your question has been deleted successfully");
       } else if (type === "Answer") {
          // Call API to delete answer
+         await deleteAnswer({ answerId: itemId });
 
          toast.success("Your answer has been deleted successfully");
       }
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -8,12 +8,16 @@ import action from "../handlers/action";
 import handleError from "../handlers/error";
 import {
    AskQuestionSchema,
+   DeleteAnswerSchema,
+   DeleteQuestionSchema,
    EditQuestionSchema,
    GetQuestionSchema,
    IncrementViewsSchema,
    PaginatedSearchParamsSchema,
 } from "../validations";
 import dbConnect from "../mongoose";
+import { Answer, Collection, Vote } from "@/database";
+import { revalidatePath } from "next/cache";
 
 export async function createQuestion(params: CreateQuestionParams): Promise<ActionResponse<Question>> {
    const validationResult = await action({
@@ -299,6 +303,116 @@ export async function getHotQuestions(): Promise<ActionResponse<Question[]>> {
          success: true,
          data: JSON.parse(JSON.stringify(questions)),
       };
+   } catch (error) {
+      return handleError(error) as ErrorResponse;
+   }
+}
+
+export async function deleteQuestion(params: DeleteQuestionParams): Promise<ActionResponse> {
+   const validationResult = await action({
+      params,
+      schema: DeleteQuestionSchema,
+      authorize: true,
+   });
+
+   if (validationResult instanceof Error) {
+      return handleError(validationResult) as ErrorResponse;
+   }
+
+   const { questionId } = validationResult.params!;
+   const { user } = validationResult.session!;
+
+   // Create a Mongoose Session
+   const session = await mongoose.startSession();
+
+   try {
+      session.startTransaction();
+
+      const question = await Question.findById(questionId).session(session);
+      if (!question) throw new Error("Question not found");
+
+      if (question.author.toString() !== user?.id) throw new Error("You are not authorized to delete this question");
+
+      // Delete references from collection
+      await Collection.deleteMany({ question: questionId }).session(session);
+
+      // Delete references from TagQuestion collection
+      await TagQuestion.deleteMany({ question: questionId }).session(session);
+
+      // For all tags of Question, find them and reduce their count
+      if (question.tags.length > 0) {
+         await Tag.updateMany({ _id: { $in: question.tags } }, { $inc: { questions: -1 } }, { session });
+      }
+
+      // Remove all votes of the question
+      await Vote.deleteMany({
+         actionId: questionId,
+         actionType: "question",
+      }).session(session);
+
+      // Remove all answers and their votes of the question
+      const answers = await Answer.find({ question: questionId }).session(session);
+
+      if (answers.length > 0) {
+         await Answer.deleteMany({ question: questionId }).session(session);
+
+         await Vote.deleteMany({
+            actionId: { $in: answers.map((answer) => answer.id) },
+            actionType: "answer",
+         }).session(session);
+      }
+
+      // Delete question
+      await Question.findByIdAndDelete(questionId).session(session);
+
+      // Commit transaction
+      await session.commitTransaction();
+      session.endSession();
+
+      // Revalidate to reflect immediate changes on UI
+      revalidatePath(`/profile/${user?.id}`);
+
+      return { success: true };
+   } catch (error) {
+      await session.abortTransaction();
+      session.endSession();
+
+      return handleError(error) as ErrorResponse;
+   }
+}
+
+export async function deleteAnswer(params: DeleteAnswerParams): Promise<ActionResponse> {
+   const validationResult = await action({
+      params,
+      schema: DeleteAnswerSchema,
+      authorize: true,
+   });
+
+   if (validationResult instanceof Error) {
+      return handleError(validationResult) as ErrorResponse;
+   }
+
+   const { answerId } = validationResult.params!;
+   const { user } = validationResult.session!;
+
+   try {
+      const answer = await Answer.findById(answerId);
+      if (!answer) throw new Error("Answer not found");
+
+      if (answer.author.toString() !== user?.id) throw new Error("You're not allowed to delete this answer");
+
+      // reduce the question answers count
+      await Question.findByIdAndUpdate(answer.question, { $inc: { answers: -1 } }, { new: true });
+
+      // delete votes associated with answer
+      await Vote.deleteMany({ actionId: answerId, actionType: "answer" });
+
+      // delete the answer
+      await Answer.findByIdAndDelete(answerId);
+
+      revalidatePath(`/profile/${user?.id}`);
+
+      return { success: true };
    } catch (error) {
       return handleError(error) as ErrorResponse;
    }
